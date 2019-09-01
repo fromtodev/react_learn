4c243fdb267e2ced8bc95066f4cf52e55f8deda0
fromtodev <fromtodev@gmail.com>
2019-09-01 04:11:07 +0300

    changes in part2(ex part 1 extended)

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,13 +1,39 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Display = ({text, value}) => <p>{text}: {value}</p>
+const Display = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const Button = ({text, onClick}) => {
     return (
         <button onClick = {onClick}>
             {text}
         </button>
+    )
+}
+
+const Statistics = ({good, bad, neutral}) => {
+    let percentGood = good / (good + neutral + bad)
+    let average = (good * 1 + bad * (-1)) / (good + neutral + bad)
+    if (isNaN(percentGood) || isNaN(average)){
+        return(
+            <div>
+                No statistics available
+            </div>
+        )
+    }
+    return(
+        <div>
+            <table>
+                <tbody>
+                    <Display value = {good} text = 'Good' />
+                    <Display value = {bad} text = 'Bad' />
+                    <Display value = {neutral} text = 'Neutral' />
+                    <Display text = 'Average: ' value = {average} />
+                    <Display text = 'Percantage of positive: ' value = {percentGood} />
+                </tbody>
+            </table>
+
+        </div>
     )
 }
 
@@ -21,9 +47,7 @@ const App = () => {
             <Button text = 'Good' onClick = {() => setGood(good + 1)} />
             <Button text = 'Bad' onClick = {() => setBad(bad + 1)} />
             <Button text = 'Neutral' onClick = {() => setNeutral(neutral + 1)} />
-            <Display value = {good} text = 'Good' />
-            <Display value = {bad} text = 'Bad' />
-            <Display value = {neutral} text = 'Neutral' />
+            <Statistics good = {good} bad = {bad} neutral = {neutral} />
         </div>
     )
 }
